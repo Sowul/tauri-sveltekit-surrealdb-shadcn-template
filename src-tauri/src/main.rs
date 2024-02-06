decbfda0caf6264b5b57fffc1272a8bbad1c0445
Sowul <bartosz.sowul@pm.me>
2024-02-06 21:58:33 +0100

    feat: add db client to tauri as managed state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod error;
+use crate::error::Error;
+
+mod db;
+use crate::db::{query, DbCxn};
+
+mod model;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello from Rust, {}!", name)
+    format!("Hello from Rust, {}!", name)
 }
 
-fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let filename = "bases\\test.db";
+    let client = DbCxn::init(filename).await?;
+
+    tauri::Builder::default()
+        .manage(client)
+        .invoke_handler(tauri::generate_handler![greet, query])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
 }
